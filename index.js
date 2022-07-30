ce727c304ccaedeafdca02be175e572027bf7d03
Paige2003 <paigebmx66@gmail.com>
2022-07-30 21:12:11 +1000

    help

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// API https://www.omdbapi.com/?apikey=e9fab4fe&s=${val}
+const movieListEl = document.querySelector('.movies');
+
+async function search(event){
+    const val = event.target.value
+    renderMovie(val)
+}
+
+async function searchWithButton(event){
+    const val = event.target.value
+    renderMovie(val)
+}
+
+async function renderMovie(val){
+    const movies = await fetch(`https://www.omdbapi.com/?apikey=e9fab4fe&s=${val}`);
+    const moviesData = await movies.json()
+   
+    movieListEl.innerHTML = [moviesData].map(movie => movieHTML(movie)).join('');
+    
+}
+
+function movieHTML(movie){
+    return `<div class="movies">
+        <figure class="movie__img--wrapper">
+            <img class="movie__img" src="${movie.Poster}" alt="">
+        </figure>
+        <h2 class="movie__title">${movie.Title}, ${movie.Year}</h2>
+    </div>`;
+}
+
